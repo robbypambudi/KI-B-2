7cca31d6e676cb9fc69f4b6fe4840512c72e2b6c
Robby Pambudi <robby.pambudi10@gmail.com>
2024-10-30 20:25:29 +0700

    initial DES

---
+++ DES/__init__.py
@@ -0,0 +1,19 @@
+from .decryption import decryption
+from .encryption import encryption
+from .helper import str_to_bin
+
+class DES:
+    def __init__(self, key):
+        self.key = key
+        
+
+    def encrypt(self, plaintext):
+        enc = encryption(plaintext)
+        # Save the encrypted text to a file
+        with open('encrypted.txt', 'w', encoding='utf-8') as f:
+            f.write(enc)
+        return enc
+
+    def decrypt(self, ciphertext):
+        enc_to_binary = str_to_bin(ciphertext)
+        return decryption(enc_to_binary)
